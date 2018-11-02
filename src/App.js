fd2280eee3360f7462c4e3895b6af7bf540cde34
Yxizzy <sylvia.onwukwe@gmail.com>
2018-11-02 17:40:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Charts from './Charts';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <Charts />
+      </div>
+    );
+  }
+}
+
+export default App;
